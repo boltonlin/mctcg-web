64a28a265802d13a28030ebe202aaf086d115540
boltonlin <boltonlinbo@gmail.com>
2023-03-01 11:37:30 -0800

    reordered for reference

--- server/db/cardModel.ts
+++ server/db/cardModel.ts
@@ -5,26 +5,29 @@ const cardSchema = new mongoose.Schema({
   _id: String,
   code: String,
   class: String,
-  linkedCard: String,
+  productSet: String,
+  productSetNumber: Number,
+  deckSet: String,
   title: String,
   ctype: String,
-  traits: String, // TODO: transform to array, problem 'S.H.I.E.L.D.'
-  abilities: {
-    type: [Number],
-    default: undefined,
-  },
   text: String,
+  url: String,
+  imagesrc: String,
+  // below are optional
+  deckSetNumber: Number,
+  linkedCard: String,
+  traits: String, // TODO: transform to array, problem 'S.H.I.E.L.D.'
   cost: Number,
   hitPoints: Number,
   resourceEnergy: Number,
   resourceMental: Number,
   resourcePhysical: Number,
   resourceWild: Number,
-  scheme: Number,
-  thwart: Number,
   attack: Number,
+  thwart: Number,
   defense: Number,
   recovery: Number,
+  scheme: Number,
   attackConsequential: Number,
   thwartConsequential: Number,
   boostIcons: Number, // star icons are NOT considered boost icons, and will be in abilities and text fields
@@ -33,14 +36,12 @@ const cardSchema = new mongoose.Schema({
   acclerationFactor: Number,
   targetThreat: Number,
   stageNumber: Number,
-  productSet: String,
-  productSetNumber: Number,
-  deckSet: String,
-  deckSetNumber: Number,
   unique: Boolean,
   subtitle: String,
-  url: String,
-  imagesrc: String,
+  abilities: {
+    type: [Number],
+    default: undefined,
+  },
 });
 
 const CardModel = mongoose.model('Card', cardSchema);
